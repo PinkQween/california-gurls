6c7d51759584321c497fa608828cf5262a346bdd
Hanna Skairipa <83796834+PinkQween@users.noreply.github.com>
2024-03-23 19:48:49 -0600

    error with video player
    Co-Authored-By: Olive <141564336+flamingolives@users.noreply.github.com>

--- js/audio.js
+++ js/audio.js
@@ -1,13 +1,22 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
     var playButton = document.getElementById('play-button');
     var youtubePlayer = document.getElementById('youtube-player');
 
-    playButton.addEventListener('click', function () {
-        if (youtubePlayer.paused) {
+    console.log(youtubePlayer)
+
+    youtubePlayer.volume = 0;
+
+    playButton.addEventListener('click', () => {
+        console.log("click");
+
+        if (youtubePlayer.volume == 0) {
             youtubePlayer.contentWindow.postMessage('{"event":"command","func":"' + 'playVideo' + '","args":""}', '*');
-            playButton.style.backgroundImage = 'url("../assets/mute.png")'
+            // playButton.style.backgroundImage = 'url("../assets/icons8-speaker-96.png")'
+            playButton.style.backgroundImage = '../assets/icons8-speaker-96.png'
         } else {
             youtubePlayer.contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
+            // playButton.style.backgroundImage = 'url("../assets/mute.png")'
+            playButton.style.backgroundImage = '../assets/mute.png'
         }
     });
 });
